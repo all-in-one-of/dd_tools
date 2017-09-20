d38c39ca69092418f56eeaa7349ee006ee17aa3a
j.fabbris <julienfabbris@gmail.com>
2017-09-20 16:38:35 +0200

    On peut maintenant exporter et importer plusieurs objects en meme temps

--- dd_houdini_toolbox/tools/import_object_from_clipboard/script.py
+++ dd_houdini_toolbox/tools/import_object_from_clipboard/script.py
@@ -24,94 +24,89 @@ def addSceneWirecolorVisualizer():
         geoviewport = hou.ui.paneTabOfType( hou.paneTabType.SceneViewer ).curViewport()
         wirecolor_vis.setIsActive( True, viewport=geoviewport )
 
-error_count = 0
+from_filename = ''
+name = ''
+object_id = 0
+wirecolor = ( 0, 0, 0 )
+handle = 0
 
 clipboard = QtWidgets.QApplication.clipboard()
 text = clipboard.text()
 lines = text.splitlines()
 
-filename = ""
-name = ""
-object_id = 0
-wirecolor = ( 0, 0, 0 )
-handle = 0
+#error_count = 0
 
-if lines.count > 1:
-    if lines[0].startswith( '#' ):
-        type = lines[0][1:]
+if lines[0] == '#abc_export':
+    geo_dir = hou.expandString('$HIP') + '/geo/'
 
-    if type == 'abc_export':
-        for line in lines[1:]:
-            if line.startswith( 'filename=' ):
-                filename = line[9:]
-            elif line.startswith( 'name=' ):
-                name = line[5:]
-            elif line.startswith( 'object_id=' ):
-                object_id = eval( line[10:] )
-            elif line.startswith( 'wirecolor=' ):
-                wirecolor = eval( line[10:] )
-            elif line.startswith( 'handle=' ):
-                handle = eval( line[7:] )
+    if not os.path.exists(geo_dir):
+        os.makedirs(geo_dir)
 
-        #copy file to $HIP folder
-        geo_dir = hou.expandString( '$HIP' ) + '/geo/'
+    for line in lines[1:]:
+        ls = line.split(',', 1)
+        if len(ls) == 2:
+            if ls[0] == 'name':
+                name = ls[1]
+            elif ls[0] == 'object_id':
+                object_id = eval( ls[1] )
+            elif ls[0] == 'wirecolor':
+                wirecolor = eval( ls[1] )
+            elif ls[0] == 'handle':
+                handle = eval( ls[1] )
+            elif ls[0] == 'filename':
+                from_filename = ls[1]
 
-        if not os.path.exists( geo_dir ):
-            os.makedirs( geo_dir )
+                if os.path.isfile(from_filename):
+                    to_filename = geo_dir + name + ".abc"
 
-        geo_filename = geo_dir + name + ".abc"
+                    try:
+                        shutil.move(from_filename, to_filename)
+                    except IOError:
+                        os.chmod( to_filename, 777 )  # ?? still can raise exception
+                        shutil.move(from_filename, to_filename)
 
-        try:
-            shutil.copy2( filename, geo_filename ) #copyFile
-        except IOError:
-            os.chmod( geo_filename, 777 )  # ?? still can raise exception
-            shutil.copy2( filename, geo_filename )
-        
-        obj = hou.node( '/obj' )
+                    obj = hou.node( '/obj' )
 
-        geo = obj.node(name)
-        if geo == None:
-            geo = obj.createNode( 'geo' , name )
-            geo.moveToGoodPosition()
-            geo.parm( 'scale' ).set( 0.01 )
-            #geo.setSelected(True, True)
+                    geo = obj.node(name)
+                    if geo == None:
+                        geo = obj.createNode( 'geo' , name )
+                        geo.moveToGoodPosition()
+                        geo.parm( 'scale' ).set( 0.01 )
+                        addVRayObjectIdParamTemplate( geo )
+                    geo.parm( 'vray_objectID' ).set( object_id )
+                    geo.parm( 'use_dcolor' ).set( True )
+                    geo.parm( 'dcolorr' ).set( wirecolor[0] )
+                    geo.parm( 'dcolorg' ).set( wirecolor[1] )
+                    geo.parm( 'dcolorb' ).set( wirecolor[2] )
 
-            addVRayObjectIdParamTemplate( geo )
+                    file = geo.node( 'file1' )
+                    if file != None:
+                        file.destroy()
 
-        geo.parm( 'vray_objectID' ).set( object_id )
-        geo.parm( 'use_dcolor' ).set( True )
-        geo.parm( 'dcolorr' ).set( wirecolor[0] )
-        geo.parm( 'dcolorg' ).set( wirecolor[1] )
-        geo.parm( 'dcolorb' ).set( wirecolor[2] )
+                    alembic = geo.node( 'alembic1' )
+                    if alembic == None:
+                        alembic = geo.createNode( 'alembic' )
+                        alembic.moveToGoodPosition()
+                    alembic.parm( 'fileName' ).set( '$HIP/geo/' + name + ".abc" )
+                    alembic.parm( 'reload' ).pressButton()
 
-        file = geo.node( 'file1' )
-        if file != None:
-            file.destroy()
+                    convert = geo.node( 'convert1' )
+                    if convert == None:
+                        convert = alembic.createOutputNode( 'convert' )
 
-        alembic = geo.node( 'alembic1' )
-        if alembic == None:
-            alembic = geo.createNode( 'alembic' )
-            alembic.moveToGoodPosition()
+                    properties = geo.node( 'properties' )
+                    if properties == None:
+                        properties = convert.createOutputNode( 'attribwrangle', 'properties' )
+                        properties.parm( 'class' ).set( 0 )
+                        properties.setDisplayFlag(True)
+                        properties.setRenderFlag(True)
+                    properties.parm( 'snippet' ).set( 'v@wirecolor = set(' + str( wirecolor[0] ) + ', ' + str( wirecolor[1] ) + ', ' + str( wirecolor[2] ) + ');\ni@handle = ' + str( handle ) + ';' )
 
-        alembic.parm( 'fileName' ).set( geo_filename )
-        alembic.parm( 'reload' ).pressButton()
+                else:
+                    print( 'cannot import ' + name + ', cannot access temporary .abc file' )
 
-        convert = geo.node( 'convert1' )
-        if convert == None:
-            convert = alembic.createOutputNode( 'convert' )
+    addSceneWirecolorVisualizer()
 
-        properties = geo.node( 'properties' )
-        if properties == None:
-            properties = convert.createOutputNode( 'attribwrangle', 'properties' )
-            properties.parm( 'class' ).set( 0 )
-            properties.setDisplayFlag(True)
-            properties.setRenderFlag(True)
-
-        properties.parm( 'snippet' ).set( 'v@wirecolor = set(' + str( wirecolor[0] ) + ', ' + str( wirecolor[1] ) + ', ' + str( wirecolor[2] ) + ');\ni@handle = ' + str( handle ) + ';' )
-
-        addSceneWirecolorVisualizer()
-
-    else:
-        print( 'cannot import object from clipboad, wrong type!' )
 else:
-    print( 'nothing to import' )
+    print( 'cannot import object from clipboad, wrong type!' )
+
